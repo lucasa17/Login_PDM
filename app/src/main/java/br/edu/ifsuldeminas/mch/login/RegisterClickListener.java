53b075820150b5cae60515f21bc4d92ccd9b021f
Lucas Silva da Rosa <134516774+lucasa17@users.noreply.github.com>
2026-04-19 19:33:31 -0300

    Atualizar o RegisterClickListener.java

--- app/src/main/java/br/edu/ifsuldeminas/mch/login/RegisterClickListener.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/login/RegisterClickListener.java
@@ -1,12 +1,15 @@
-package br.edu.ifsuldeminas.mch.login;
+package br.edu.ifsuldeminas.mch.pdm.login;
 
 import android.view.View;
 import android.widget.Toast;
 
-public class RegisterClickListener implements View.OnClickListener{
+public class RegisterClickListener implements View.OnClickListener {
 
     @Override
-    public void onClick(View v){
-        Toast.makeText(v.getContext(), "Botão Cadastro Clicado!", Toast.LENGTH_LONG).show();
+    public void onClick(View v) {
+        Toast.makeText(
+                v.getContext(),
+                "Botão Cadastrar Clicado!",
+                Toast.LENGTH_LONG).show();
     }
 }
