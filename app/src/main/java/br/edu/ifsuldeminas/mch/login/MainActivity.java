f7b9329baf3ca4317516117980cc8e33f1e9e9da
Lucas Silva da Rosa <134516774+lucasa17@users.noreply.github.com>
2026-04-19 19:33:11 -0300

    Atualizar o MainActivity.java

--- app/src/main/java/br/edu/ifsuldeminas/mch/login/MainActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/login/MainActivity.java
@@ -1,4 +1,4 @@
-package br.edu.ifsuldeminas.mch.login;
+package br.edu.ifsuldeminas.mch.pdm.login;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,14 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String USER_NAME = "Lucas";
-    private static final String PW = "123";
+    private static final String USER_NAME = "emerson";
+    private static final String PW = "admin";
+
     private static final String LOG_TAG = "login_activity_main";
     private Button buttonLogin;
     private Button buttonRegister;
@@ -24,20 +29,28 @@ public class MainActivity extends AppCompatActivity {
     private EditText editTextUser;
     private EditText editTextPW;
 
-    private ActivityResultLauncher<String> startWelcomeActLauncher = registerForActivityResult(new SimpleContract(), new ActivityResultCallback<String>() {
-        @Override
-        public void onActivityResult(String result) {
-            if(result == null || result.isBlank()){
-                return;
-            }
-            Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
-        }
-    });
+    private ActivityResultLauncher<String> startWelcomeActLauncher =
+            registerForActivityResult(new SimpleContract(),
+                    new ActivityResultCallback<String>() {
+                @Override
+                public void onActivityResult(String result) {
+                    if (result == null || result.isBlank())
+                        return;
+
+                    Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         buttonLogin = findViewById(R.id.buttonLoginId);
         buttonRegister = findViewById(R.id.buttonRegisterId);
@@ -45,23 +58,22 @@ public class MainActivity extends AppCompatActivity {
         editTextUser = findViewById(R.id.textInputEditTextUserId);
         editTextPW = findViewById(R.id.textInputEditTextPwId);
 
-        // Classe interna
-        buttonLogin.setOnClickListener(new View.OnClickListener(){
+        // Classe anônima
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userName = editTextUser.getText().toString();
                 String userPW = editTextPW.getText().toString();
 
-                if(USER_NAME.equals(userName) && PW.equals(userPW)){
-                    Intent welcomeIntent = new Intent(getApplicationContext(), WelcomeActivity.class);
-
-                    welcomeIntent.putExtra(userName, userPW);
-
-                    startActivity(welcomeIntent);
-                }else{
-                    Toast toast = Toast.makeText(getBaseContext(), R.string.invalid_user_or_pw, Toast.LENGTH_LONG);
+                if (USER_NAME.equals(userName) && PW.equals(userPW)) {
+                    // Via launcher
+                    startWelcomeActLauncher.launch(userName);
+                } else {
+                    Toast toast = Toast.makeText(
+                            getBaseContext(),
+                            R.string.invalid_user_or_pw,
+                            Toast.LENGTH_LONG);
                     toast.show();
-
                     editTextUser.requestFocus();
                 }
             }
@@ -70,47 +82,44 @@ public class MainActivity extends AppCompatActivity {
         // Classe externa
         buttonRegister.setOnClickListener(new RegisterClickListener());
 
-        // Lambdas com interface SAM
+        // Lambdas com interfaces SAM
         buttonForgotPW.setOnClickListener((View view) -> {
-            Toast.makeText(view.getContext(), R.string.button_forgot_pw_clicked, Toast.LENGTH_SHORT).show();
+            Toast.makeText(
+                    view.getContext(),
+                    R.string.button_forgot_pw_clicked,
+                    Toast.LENGTH_SHORT).show();
         });
 
-        Log.i(LOG_TAG,"Método onCreate da MainActivity executou com sucesso!");
+        Log.i(LOG_TAG, "Método onCreate da MainActivity executou com sucesso!");
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
-        Log.i(LOG_TAG, "MainActivity foi fechada");
+        Log.i(LOG_TAG, "ActivityMain foi fechada.");
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        Log.i(LOG_TAG, "ActivityMain passou pelo onStart");
+        Log.i(LOG_TAG, "ActivityMain passou pelo onStart.");
     }
 
     @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i(LOG_TAG, "ActivityMain passou pelo onRestart");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(LOG_TAG, "ActivityMain passou pelo onPause");
-    }
-
-    @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
-        Log.i(LOG_TAG, "Activity passou pelo onResume");
+        Log.i(LOG_TAG, "ActivityMain passou pelo onResume.");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(LOG_TAG, "Activity passou pelo onStop");
+        Log.i(LOG_TAG, "ActivityMain passou pelo onStop.");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "ActivityMain passou pelo onPause.");
     }
 }
