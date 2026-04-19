744eea29644e35cecb54928778d6932d08105e94
Lucas Silva da Rosa <134516774+lucasa17@users.noreply.github.com>
2026-04-19 19:34:16 -0300

    Atualizar o WelcomeActivity.java

--- app/src/main/java/br/edu/ifsuldeminas/mch/login/WelcomeActivity.java
+++ app/src/main/java/br/edu/ifsuldeminas/mch/login/WelcomeActivity.java
@@ -1,5 +1,8 @@
-package br.edu.ifsuldeminas.mch.login;
+package br.edu.ifsuldeminas.mch.pdm.login;
 
+import android.app.Activity;
+import android.app.ComponentCaller;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -7,9 +10,9 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -18,17 +21,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import org.jspecify.annotations.NonNull;
-
 import java.text.MessageFormat;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     private Button buttonTakePicture;
     private ImageView imageViewPicture;
+
     private Intent resultIntent;
+
+    private static final int REQUEST_PICTURE_CODE = 987654;
     private static final String RESULT_KEY = "resultado";
-    private static final int REQUEST_PICTURE_CODE = 987654321;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,8 @@ public class WelcomeActivity extends AppCompatActivity {
         Intent intentQueChamou = getIntent();
         String userName = intentQueChamou.getStringExtra("user_name");
 
-        String finalMessage = MessageFormat.format("{0} {1}!", getString(R.string.welcome_message), userName);
+        String finalMessage = MessageFormat.format("{0} {1}!",
+                getText(R.string.welcome_message), userName);
 
         View viewLayout = findViewById(R.id.activity_welcome_id);
         Snackbar snackbar = Snackbar.make(viewLayout, finalMessage, Snackbar.LENGTH_LONG);
@@ -58,33 +62,34 @@ public class WelcomeActivity extends AppCompatActivity {
             startActivityForResult(takePicIntent, REQUEST_PICTURE_CODE);
         });
 
-
         resultIntent = new Intent();
-        resultIntent.putExtra(RESULT_KEY, "Não bateu a foto");
-        setResult(WelcomeActivity.RESULT_OK, resultIntent);
+        resultIntent.putExtra(RESULT_KEY, "Não bateu a foto.");
+        setResult(Activity.RESULT_OK, resultIntent);
     }
 
-
-    // Dar suporte ao Botão up navigation (<--)
+    // Dar suporte ao botão Up Navigation (<--)
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public void onActivityResult(int requestCode,
+                                 int resultCode,
+                                 @Nullable Intent data,
+                                 @NonNull ComponentCaller caller) {
+        super.onActivityResult(requestCode, resultCode, data, caller);
 
-        if (requestCode == REQUEST_PICTURE_CODE){
-            if(data != null){
+        if (requestCode == REQUEST_PICTURE_CODE) {
+            if (data != null) {
                 Bundle extras = data.getExtras();
 
-                if (extras != null){
+                if (extras != null) {
                     Bitmap image = (Bitmap) extras.get("data");
                     imageViewPicture.setImageBitmap(image);
 
-                    resultIntent.putExtra(RESULT_KEY, "Bateu a foto");
+                    resultIntent.putExtra(RESULT_KEY, "Bateu a foto.");
                 }
             }
         }
